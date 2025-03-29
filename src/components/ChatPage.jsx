c6f3b922e1ab4927e5186ca2e8ca19d29443b0e1
oezekielanim <83212938+oezekielanim@users.noreply.github.com>
2025-03-29 17:30:41 +0000

    Update ChatPage.jsx

--- src/components/ChatPage.jsx
+++ src/components/ChatPage.jsx
@@ -44,7 +44,7 @@ export default function ChatbotUI() {
       //   headers: { "Content-Type": "application/json" },
       //   body: JSON.stringify({ question: input }),
       // });
-      const response = await fetch("https://pfrimpong-hr-policy-bot.hf.space/docs", {
+      const response = await fetch("https://pfrimpong-hr-policy-bot.hf.space/ask", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question: input }),
