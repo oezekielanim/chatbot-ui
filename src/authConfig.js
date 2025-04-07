36bcdd1b8599f490d9d603db7202b861ea75c1c3
oezekielanim <83212938+oezekielanim@users.noreply.github.com>
2025-04-07 11:20:57 +0000

    dfd

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
     auth: {
       clientId: "YOUR_CLIENT_ID", // From Azure
       authority: "https://login.microsoftonline.com/YOUR_TENANT_ID", // From Azure
-      redirectUri: "https://chatbot-ui-pearl-psi.vercel.app/", // Your app URI
+      redirectUri: "https://chatbot-ui-pearl-psi.vercel.app/chatpage", // Your app URI
     },
     cache: {
       cacheLocation: "sessionStorage", // or "localStorage"
--- src/msalConfig.js
+++ src/msalConfig.js
@@ -3,7 +3,7 @@ export const msalConfig = {
     auth: {
       clientId: "cb5e355a-66e4-4050-bbea-e58adf70ee48", // 🔁 Replace this
       authority: "https://login.microsoftonline.com/5dd2e9cf-7e8e-47a6-bb1e-592762211965", // 🔁 Replace this
-      redirectUri: "https://chatbot-ui-pearl-psi.vercel.app/", // or your Vercel URL
+      redirectUri: "https://chatbot-ui-pearl-psi.vercel.app/chatpage", // or your Vercel URL
     },
   };
   
